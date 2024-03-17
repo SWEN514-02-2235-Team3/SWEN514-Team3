4f94ee486233e9ded397eea361a71b9b24b23c5f
Jin Moon <jym2584@rit.edu>
2024-03-17 18:54:00 -0400

    deploy and teardown

---
+++ terraform/aws_provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region                      = "${var.region}"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  access_key                  = "AKIA47CRXH6B67LYT56Z"
+  secret_key                  = "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"
+
+}
