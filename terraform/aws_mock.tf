cede8a2a055764323258d1281842907bd30ca3fb
Jin Moon <jym2584@rit.edu>
2024-03-17 15:57:53 -0400

    woops :)

--- terraform/aws_mock.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region                      = "${var.region}"
-  access_key                  = "AKIA47CRXH6B67LYT56Z"
-  secret_key                  = "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"
-
-  
-}
