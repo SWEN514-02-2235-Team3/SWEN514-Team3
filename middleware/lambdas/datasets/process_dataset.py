554d0eab85700566a3b484353a9ce61395e799bd
Jin Moon <jym2584@rit.edu>
2024-03-23 22:09:28 -0400

    debug code

--- middleware/lambdas/datasets/process_dataset.py
+++ middleware/lambdas/datasets/process_dataset.py
@@ -1,1 +1,20 @@
-print("Hello test!")
+import boto3
+import logging
+
+# boto3 S3 initialization
+s3_client = boto3.client("s3")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+   # event contains all information about uploaded object
+   print("Event :", event)
+
+   # Bucket Name where file was uploaded
+   source_bucket_name = event['Records'][0]['s3']['bucket']['name']
+
+   # Filename of object (with path)
+   file_key_name = event['Records'][0]['s3']['object']['key']
+   
+   logger.info(source_bucket_name)
+   logger.info(file_key_name)
