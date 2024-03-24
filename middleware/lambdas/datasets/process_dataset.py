0af0a0aca2bf96572931a0457f72160b56148e22
Jin Moon <jym2584@rit.edu>
2024-03-23 23:02:23 -0400

    added boilerplat ecode

--- middleware/lambdas/datasets/process_dataset.py
+++ middleware/lambdas/datasets/process_dataset.py
@@ -1,20 +1,45 @@
 import boto3
 import logging
-
-# boto3 S3 initialization
-s3_client = boto3.client("s3")
+import csv
+import io
+# Setup
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Boto Setup
+s3_client = boto3.client("s3")
+comprehend_client = boto3.client("comprehend")
+dynamodb_client = boto3.client("dynamodb")
+
+# Lambda trigger
 def handler(event, context):
-   # event contains all information about uploaded object
-   print("Event :", event)
-
-   # Bucket Name where file was uploaded
-   source_bucket_name = event['Records'][0]['s3']['bucket']['name']
-
-   # Filename of object (with path)
-   file_key_name = event['Records'][0]['s3']['object']['key']
+    bucket_source = event['Records'][0]['s3']['bucket']['name'] # Bucket Name where file was uploaded
+    dataset_filename = event['Records'][0]['s3']['object']['key'] # Filename of object (with path)
+    
+    # Debug print
+    to_print = f"RECIEVED DATASET `{dataset_filename}` FROM BUCKET `{bucket_source}`"
+    print("-" * len(to_print))
+    print(to_print)
+    print("-" * len(to_print))
    
-   logger.info(source_bucket_name)
-   logger.info(file_key_name)
+   # Parse through file
+    file = s3_client.get_object(Bucket=bucket_source, Key=dataset_filename) # get file from s3 bucket
+    content = file['Body'].read().decode('utf-8') # read contents
+    reader = csv.reader(io.StringIO(content)) # pass into csv reader
+    
+    i = 0 # mostly for debugging
+    for row in reader:
+            #   For each line in file:
+            #       Get comment from csv file
+            #       Analyze text data using sentiment analysis
+            #       Extract the sentiment
+            #       Store results in DymanoDB
+        
+        
+        print(row)
+        i+=1
+        
+        if (i >= 10):
+            break
+        
+       
