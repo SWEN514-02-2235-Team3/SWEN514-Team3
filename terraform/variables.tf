6872af27c028860ef604ac2456293d7bb24ff122
Jin Moon <jym2584@rit.edu>
2024-03-24 20:59:05 -0400

    added input for dataset selection

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,15 +8,25 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "dataset_selection" {
+  description = "Specify the type of dataset to use: 'datasets_test' or 'datasets_full'"
+  type        = string
+  default     = "datasets_test"
+
+  validation {
+    condition     = var.dataset_selection == "datasets_test" || var.dataset_selection == "datasets_full"
+    error_message = "Invalid value for dataset selection. Allowed values are 'datasets_test' or 'datasets_full'"
+  }
+}
 /*
-Dynamically retrieve csv files from data/ on local repository
+Dynamically retrieve csv files from data/$var.dataset_selection on local repository
 */
 locals {
   data_dirs = ["reddit", "youtube", "twitter"]
 
   csv_files = flatten([
     for dir in local.data_dirs: [
-      for file in fileset("../data/${dir}", "*.csv") : {
+      for file in fileset("../data/${var.dataset_selection}/${dir}", "*.csv") : {
         path = "../data/${dir}/${file}"
         key  = "${dir}/${file}"
       }
