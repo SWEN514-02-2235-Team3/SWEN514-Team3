ae089f422917fa2d063f91871b01e0d261f8cb69
sd1259 <78935467+sd1259@users.noreply.github.com>
2024-03-28 22:59:17 -0400

    get lambda completed

---
+++ backend/sentiments.py
@@ -0,0 +1,71 @@
+import json
+import sys
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from datetime import datetime
+
+# Initialize the DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+
+# Replace 'YOUR_TABLE_NAME' with the actual table name
+table = dynamodb.Table('SentAnalysisDataResults')
+
+
+def get_sentiments(event):
+    # Extract limit from event if present, default to 50 if not
+    query_limit = int(event.get("limit", 50))
+    query_scope = event.get("scope")
+    from_date_str = event.get("from_date")
+    to_date_str = event.get("to_date")
+    
+    # Convert date strings to datetime objects
+    from_date = datetime.strptime(from_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
+    to_date = datetime.strptime(to_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
+    
+    filter_expression = None
+    
+    if from_date and to_date:
+        filter_expression = Key('comment_date').between(from_date, to_date)
+    elif from_date:
+        filter_expression = Key('comment_date').gte(from_date)
+    elif to_date:
+        filter_expression = Key('comment_date').lte(to_date)
+    
+    # Construct query parameters
+    query_params = {
+        'Limit': query_limit
+    }
+    
+    if query_scope:
+        query_params['FilterExpression'] = Key('source').eq(query_scope)
+    
+    if filter_expression:
+        if 'FilterExpression' in query_params:
+            query_params['FilterExpression'] = query_params['FilterExpression'] & filter_expression
+        else:
+            query_params['FilterExpression'] = filter_expression
+    
+    response = table.scan(**query_params)
+    
+    items = response['Items']
+    # for item in items:
+    #     print(item)
+    return items    
+
+def lambda_handler(event, context):
+    
+    operation = event.get('operation')
+    
+    if operation == 'GET':
+        try:
+            sentiments = get_sentiments(event)
+            return {
+                'statusCode': 200,
+                'body': sentiments
+            }
+        except Exception as e:
+            print("Error:", e)
+            return {
+                'statusCode': 500,
+                'body': str(e)
+            }
