3d7f81a85b079cdd507ab7981a8a9f68b6fb75f1
sd1259 <78935467+sd1259@users.noreply.github.com>
2024-03-28 23:28:01 -0400

    post done

--- backend/sentiments.py
+++ backend/sentiments.py
@@ -1,16 +1,50 @@
 import json
 import sys
 import boto3
+import random
+import string
 from boto3.dynamodb.conditions import Key, Attr
 from datetime import datetime
 
-# Initialize the DynamoDB client
-dynamodb = boto3.resource('dynamodb')
 
-# Replace 'YOUR_TABLE_NAME' with the actual table name
+dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('SentAnalysisDataResults')
 
-
+def lambda_handler(event, context):
+    
+    operation = event.get('operation')
+    
+    if operation == 'GET':
+        try:
+            sentiments = get_sentiments(event)
+            return {
+                'statusCode': 200,
+                'body': sentiments
+            }
+        except Exception as e:
+            print("Error:", e)
+            return {
+                'statusCode': 500,
+                'body': str(e)
+            }
+    elif operation == 'POST':
+        try:
+            create_sentiment(event)
+            return {
+                'statusCode': 200,
+                'body': 'Entry created successfully'
+            }
+        except Exception as e:
+            print("Error:", e)
+            return {
+                'statusCode': 500,
+                'body': str(e)
+            }
+    elif operation == 'PUT':
+        pass
+    elif operation == 'DELETE':
+        pas
+    
 def get_sentiments(event):
     # Extract limit from event if present, default to 50 if not
     query_limit = int(event.get("limit", 50))
@@ -18,17 +52,17 @@ def get_sentiments(event):
     from_date_str = event.get("from_date")
     to_date_str = event.get("to_date")
     
-    # Convert date strings to datetime objects
-    from_date = datetime.strptime(from_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
-    to_date = datetime.strptime(to_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
-    
     filter_expression = None
     
-    if from_date and to_date:
+    if from_date_str and to_date_str:
+        from_date = datetime.strptime(from_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
+        to_date = datetime.strptime(to_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
         filter_expression = Key('comment_date').between(from_date, to_date)
-    elif from_date:
+    elif from_date_str:
+        from_date = datetime.strptime(from_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
         filter_expression = Key('comment_date').gte(from_date)
-    elif to_date:
+    elif to_date_str:
+        to_date = datetime.strptime(to_date_str, "%Y-%m-%d").strftime('%Y-%m-%d')
         filter_expression = Key('comment_date').lte(to_date)
     
     # Construct query parameters
@@ -50,22 +84,25 @@ def get_sentiments(event):
     items = response['Items']
     # for item in items:
     #     print(item)
-    return items    
-
-def lambda_handler(event, context):
+    return items
     
-    operation = event.get('operation')
+def create_sentiment(event):
     
-    if operation == 'GET':
-        try:
-            sentiments = get_sentiments(event)
-            return {
-                'statusCode': 200,
-                'body': sentiments
-            }
-        except Exception as e:
-            print("Error:", e)
-            return {
-                'statusCode': 500,
-                'body': str(e)
-            }
+    random_string = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
+    
+    new_id = random_string
+    new_comment = event.get("comment")
+    new_comment_date = datetime.now().strftime('%Y-%m-%d')
+    new_sentiment = event.get("sentiment")
+    new_source = event.get("source")
+    
+    new_entry = {
+        'id': "1",
+        'comment': new_comment,
+        'comment_date': new_comment_date,
+        'sentiment': new_sentiment,
+        'source': new_source
+    }
+    
+    table.put_item(Item=new_entry)
+    
