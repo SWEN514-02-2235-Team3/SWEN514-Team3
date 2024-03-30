0a17b7d2463c8057f4d14f9c19d9de192aa46723
Nick <nrg5223@g.rit.edu>
2024-03-30 12:48:26 -0400

    added json with configuration of api gateway made using AWS console.  Needs to be converted to tf code.  Added tf code outline.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,50 +1,61 @@
-# /************************
+/************************
 
-# API Gateway Code Structure
+API Gateway Code Structure
 
-# Main API Gateway (sa_api_gateway)
-#     /sentiments
-#         GET
-#             headers: {
-#                 "source" (OPTIONAL): Filter source of the sentiment (i.e. twitter, reddit, youtube)
-#                 "sentiment" (OPTIONAL): Filter by sentiment (POSITIVE/NEGATIVE/NEUTRAL/MIXED)
-#             }
+Main API Gateway (sa_api_gateway)
+    /sentiments
+        GET
+            headers: {
+                "source" (OPTIONAL): Filter source of the sentiment (i.e. twitter, reddit, youtube)
+                "sentiment" (OPTIONAL): Filter by sentiment (POSITIVE/NEGATIVE/NEUTRAL/MIXED)
+            }
 
-# ************************/
+************************/
 
-# /*
-#     Main API Gateway
-# */
-# resource "aws_api_gateway_rest_api" "sa_api_gateway" {
-#   name        = "sa_api_gateway"
-#   description = "SWEN 514 Sentiment Analysis API Gateway"
-# }
+/*
+    Main API Gateway
+*/
+resource "aws_api_gateway_rest_api" "sa_api_gateway" {
+  name        = "sa_api_gateway"
+  description = "SWEN 514 Sentiment Analysis API Gateway"
+}
 
-# /*
-#     /sentiments ENDPOINT
-# */
-# resource "aws_api_gateway_resource" "sa_endpoint_sentiments" {
+# resource "aws_api_gateway_resource" "sentiments_resource" {
 #   rest_api_id = aws_api_gateway_rest_api.sa_api_gateway.id
 #   parent_id   = aws_api_gateway_rest_api.sa_api_gateway.root_resource_id
 #   path_part   = "sentiments"
 # }
-
-# // sentiments GET 
-# resource "aws_api_gateway_method" "sa_endpoint_sentiments_get" {
+# 
+# resource "aws_api_gateway_method" "get_sentiments_method" {
 #   rest_api_id   = aws_api_gateway_rest_api.sa_api_gateway.id
-#   resource_id   = aws_api_gateway_resource.sa_endpoint_sentiments.id
+#   resource_id   = aws_api_gateway_resource.sentiments_resource.id
 #   http_method   = "GET"
 #   authorization = "NONE"
 # }
-
+# 
+# resource "aws_api_gateway_integration" "get_sentiments_integration" {
+#   rest_api_id             = aws_api_gateway_rest_api.sa_api_gateway.id
+#   resource_id             = aws_api_gateway_resource.sentiments_resource.id
+#   http_method             = aws_api_gateway_method.get_sentiments_method.http_method
+#   integration_http_method = "GET"
+#   type                    = "HTTP"
+# }
+# 
+# resource "aws_api_gateway_deployment" "sa_api_gateway_deployment" {
+#   depends_on    = [aws_api_gateway_integration.get_sentiments_integration]
+#   rest_api_id   = aws_api_gateway_rest_api.sa_api_gateway.id
+#   stage_name    = "dev"
+#   description   = "Development Stage"
+# }
+# 
 # // sentiments GET - Invoke Lambda function  
 # resource "aws_api_gateway_integration" "lambda" {
 #   rest_api_id             = aws_api_gateway_rest_api.sa_api_gateway.id
-#   resource_id             = aws_api_gateway_resource.sa_endpoint_sentiments.id
-#   http_method             = aws_api_gateway_method.sa_endpoint_sentiments_get.http_method
-
+#   resource_id             = aws_api_gateway_resource.sentiments_resource.id
+#   http_method             = aws_api_gateway_method.get_sentiments_method.http_method
+# 
 #   integration_http_method = "POST"
 #   type                    = "AWS_PROXY"
-
-#   uri                     = aws_lambda_function.example.invoke_arn
+# 
+# #   uri                     = aws_lambda_function.example.invoke_arn # Saranya can replace this with the lambda she makes -Nick
 # }
