8b406bdb3c397a93ab828ee84eaef3a9c9f5a51b
Jin Moon <jym2584@rit.edu>
2024-04-02 10:24:56 -0400

    force flag

--- setup_dependencies.bat
+++ setup_dependencies.bat
@@ -1,5 +1,5 @@
 set "CURRENT_DIR=%CD%"
 pip install -r requirements.txt
 cd "%CURRENT_DIR%\terraform\" && terraform init -upgrade
-cd "%CURRENT_DIR%\frontend\" && npm install
+cd "%CURRENT_DIR%\frontend\" && npm install --force
 pause
