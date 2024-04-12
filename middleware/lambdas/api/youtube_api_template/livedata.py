f79ed339e3949e6dd2d7d8f7af49a3bc1c4be31a
sd1259 <78935467+sd1259@users.noreply.github.com>
2024-04-12 18:54:53 -0400

    filters from event

--- middleware/lambdas/api/youtube_api_template/livedata.py
+++ middleware/lambdas/api/youtube_api_template/livedata.py
@@ -7,8 +7,8 @@ import json
 # https://developers.google.com/explorer-help/code-samples#python
 
 import os
-
 import googleapiclient.discovery
+from datetime import datetime
 
 # Your API key
 api_key = "AIzaSyBwar2tkCtOhYkkQngj6qTZuvSnyU6GuM0"
@@ -21,6 +21,22 @@ def lambda_handler(event, context):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     region = event.get('region') if event.get('region') else None
+    max_results = event.get('max_results') if event.get('max_results') else None
+
+    if event.get('date_from'):
+        date_from_str = event.get('date_from')
+        date_from = datetime.strptime(date_from_str, "%Y-%m-%d").isoformat() + "Z"
+    else:
+        date_from = None
+
+    if event.get('date_to'):
+        date_to_str = event.get('date_to')
+        date_to = datetime.strptime(date_to_str, "%Y-%m-%d").isoformat() + "Z"
+    else:
+        date_to = None
+
+    print(f"[DEBUG] date_from: {date_from}")
+    print(f"[DEBUG] date_to: {date_to}")
 
     # Build the YouTube API client using API key
     youtube = googleapiclient.discovery.build(
@@ -32,10 +48,12 @@ def lambda_handler(event, context):
         q="covid-19",
         regionCode=region,
         type="video",
-        videoType="any"
+        videoType="any",
+        maxResults=max_results,
+        publishedBefore=date_to,
+        publishedAfter=date_from
     )
     response = request.execute()
-    # print(f"[DEBUG] response: {response}")
 
     items = response['items']
     info = []
@@ -46,9 +64,11 @@ def lambda_handler(event, context):
         snippet = each['snippet']
         title = snippet['title']
         publish_time = snippet['publishTime']
+        description = snippet['description']
         item = {
             'title': title,
-            'publish_time': publish_time
+            'publish_time': publish_time,
+            'description': description
         }
         info.append(item)
 
