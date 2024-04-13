b21ede0da41dcbb50d678cd92afadd2b9ccd450d
Jin Moon <jym2584@rit.edu>
2024-04-13 15:38:05 -0400

    changes

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -34,6 +34,18 @@ resource "aws_api_gateway_resource" "sentiments_resource" {
   depends_on = [ aws_api_gateway_rest_api.sa_api_gateway ]
 }
 
+// API gateway deployment stage
+resource "aws_api_gateway_deployment" "sa_api_gateway_deployment" {
+  depends_on    = [aws_api_gateway_integration.lambda, aws_api_gateway_rest_api.sa_api_gateway]
+  rest_api_id   = aws_api_gateway_rest_api.sa_api_gateway.id
+  stage_name    = "dev"
+  description   = "Development Stage"
+  
+}
+
+/////////////////////////////////////////////////
+//////////// SENTIMENTS GET
+/////////////////////////////////////////////////
 // GET sentiments/ (method request)
 resource "aws_api_gateway_method" "get_sentiments_method" {
   rest_api_id   = aws_api_gateway_rest_api.sa_api_gateway.id
@@ -49,15 +61,6 @@ resource "aws_api_gateway_method" "get_sentiments_method" {
   }
   depends_on = [ aws_api_gateway_rest_api.sa_api_gateway ]
 
-}
-
-// API gateway deployment stage
-resource "aws_api_gateway_deployment" "sa_api_gateway_deployment" {
-  depends_on    = [aws_api_gateway_integration.lambda, aws_api_gateway_rest_api.sa_api_gateway]
-  rest_api_id   = aws_api_gateway_rest_api.sa_api_gateway.id
-  stage_name    = "dev"
-  description   = "Development Stage"
-  
 }
 
 // sentiments GET - Invoke Lambda function (integration request)
@@ -134,16 +137,16 @@ resource "aws_api_gateway_method" "youtube_live" {
 }
 
 
-// sentiments GET - Invoke Lambda function (integration request)
+// sentiments POST - Invoke Lambda function (integration request)
 resource "aws_api_gateway_integration" "youtube_live" {
   rest_api_id             = aws_api_gateway_rest_api.sa_api_gateway.id
   resource_id             = aws_api_gateway_resource.sentiments_resource.id
-  http_method             = aws_api_gateway_method.get_sentiments_method.http_method
+  http_method             = aws_api_gateway_method.youtube_live.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY" 
 
-  uri                     = aws_lambda_function.get_sentiments.invoke_arn # Saranya's lambda function
+  uri                     = aws_lambda_function.get_sentiments_youtube.invoke_arn # Saranya's lambda function
   credentials             = aws_iam_role.sa_api_gateway_role.arn
   request_parameters = {
     "integration.request.querystring.source"             = "method.request.querystring.source" # source = method.request.querystring.source
