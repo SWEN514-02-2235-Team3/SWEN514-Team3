67358084f7be1c3b16784948eac46f464425018f
Jin Moon <jym2584@rit.edu>
2024-04-14 23:50:07 -0400

    cloudwatch event + timeout threshold

--- terraform/cloudwatch_events.tf
+++ terraform/cloudwatch_events.tf
@@ -12,16 +12,11 @@ resource "aws_cloudwatch_event_rule" "api_gateway_execution" {
     PATTERN
 }
 
-variable "lambda_function_arns" {
-  type    = list(string)
-  default = [aws_lambda_function.get_sentiments.arn, aws_lambda_function.get_sentiments_youtube.arn, aws_lambda_function.lambda_s3_datasets.arn]
-}
-
 resource "aws_cloudwatch_event_target" "log_lambda_triggers" {
-  count     = length(var.lambda_function_arns)
+  count     = length([aws_lambda_function.get_sentiments.arn, aws_lambda_function.get_sentiments_youtube.arn, aws_lambda_function.lambda_s3_datasets.arn])
   rule      = aws_cloudwatch_event_rule.api_gateway_execution.name
   target_id = "InvokeLambdaFunction_${count.index}"
-  arn       = var.lambda_function_arns[count.index]
+  arn       = element([aws_lambda_function.get_sentiments.arn, aws_lambda_function.get_sentiments_youtube.arn, aws_lambda_function.lambda_s3_datasets.arn], count.index)
 
   depends_on = [ aws_lambda_function.get_sentiments, aws_lambda_function.get_sentiments_youtube, aws_lambda_function.lambda_s3_datasets ]
 }
